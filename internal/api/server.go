c5123b1c4c0185c35a963a4381e38ce3370b124a
Arif Kurniawan <arifkurniawan98@gmail.com>
2024-03-31 08:05:03 +0700

    feat: add gracefully shutdown and implementing versioning

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,11 +1,17 @@
-package app
+package api
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
 	"net/http"
-	"sync"
+	"os"
+	"os/signal"
+	"syscall"
+	"template/config"
 	"template/internal/usecase"
 	"time"
 )
@@ -15,7 +21,7 @@ type handler struct {
 	Transaction usecase.TransactionUcase
 }
 
-func Run(u usecase.UserUcase, t usecase.TransactionUcase) {
+func Run(cfg config.Config, u usecase.UserUcase, t usecase.TransactionUcase) {
 	e := echo.New()
 
 	h := handler{
@@ -35,7 +41,7 @@ func Run(u usecase.UserUcase, t usecase.TransactionUcase) {
 	}))
 
 	// Rate Limiter Configuration
-	config := middleware.RateLimiterConfig{
+	rateLimiterConfig := middleware.RateLimiterConfig{
 		Skipper: middleware.DefaultSkipper,
 		Store: middleware.NewRateLimiterMemoryStoreWithConfig(
 			middleware.RateLimiterMemoryStoreConfig{Rate: 10, Burst: 30, ExpiresIn: 3 * time.Minute},
@@ -51,30 +57,49 @@ func Run(u usecase.UserUcase, t usecase.TransactionUcase) {
 			return context.JSON(http.StatusTooManyRequests, nil)
 		},
 	}
-	e.Use(middleware.RateLimiterWithConfig(config))
-	e.POST("/register", h.RegisterUser)
-	e.POST("/login", h.LoginUser)
+	v1 := e.Group("/v1")
+	v1.Use(onlyJSONMiddleware)
+	v1.Use(middleware.RateLimiterWithConfig(rateLimiterConfig))
 
-	customer := e.Group("/customer")
+	v1.POST("/register", h.RegisterUser)
+	v1.POST("/login", h.LoginUser)
+
+	customer := v1.Group("/customer")
 	{
-		customer.Use(JWTMiddleware("secret")) // still default,can change anytime (i suggest i should placed in  .env)
+		customer.Use(JWTMiddleware(cfg.Environment.SecretKey))
 	}
-	admin := e.Group("/admin")
+	admin := v1.Group("/admin")
 	{
-		admin.Use(JWTMiddleware("secret")) // still default,can change anytime (i suggest i should placed in  .env)
+		admin.Use(JWTMiddleware(cfg.Environment.SecretKey))
 		admin.Use(AdminMiddleware)
 	}
 
-	var wg sync.WaitGroup
+	stop := make(chan os.Signal, 1)
 
-	wg.Add(1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
 	go func() {
-		defer wg.Done()
 
-		if err := e.Start(":8080"); err != nil {
-			fmt.Printf("Error starting server: %v\n", err)
+		if err := e.Start(fmt.Sprintf(":%d", cfg.API.Port)); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("Error starting server: %v\n", err)
+			os.Exit(1)
 		}
 	}()
 
-	wg.Wait()
+	<-stop
+
+	log.Println("OS SIGNAL RECEIVED")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 40*time.Second)
+
+	defer cancel()
+
+	log.Println("SHUTTING DOWN SERVER...")
+
+	if err := e.Shutdown(ctx); err != nil {
+		log.Printf("ERR SHUTTING DOWN SERVER : %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Println("SERVER GRACEFULLY STOPPED")
 }
